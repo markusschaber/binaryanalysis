d9f498590ba220abf0e160480b93e7cf0ad5888d
Matthei52 <matthei@hotmail.de>
2017-10-20 12:24:45 +0200

    Altered scorecache.py to add scores for every supported language, not just one. Added caches.config to modify scorecache.py.

--- src/maintenance/scorecache.py
+++ src/maintenance/scorecache.py
@@ -4,62 +4,140 @@
 ## Copyright 2014-2015 Armijn Hemel for Tjaldur Software Governance Solutions
 ## Licensed under Apache 2.0, see LICENSE file for details
 
+'''
+Script to initiate the score caches in the database.
+'''
+
 import sys, os, os.path
-import sqlite3
+import psycopg2, ConfigParser
 from optparse import OptionParser
 
 def main(argv):
 	alpha = 5.0
+	config = ConfigParser.ConfigParser()
 
 	parser = OptionParser()
-	parser.add_option("-d", "--database", action="store", dest="db", help="path to caching database", metavar="FILE")
+	parser.add_option("-d", "--config", action="store", dest="cfg", help="path to caching database", metavar="FILE")
 	(options, args) = parser.parse_args()
-	if options.db == None:
-		parser.error("Path to caching database")
-	if not os.path.exists(options.db):
-		print >>sys.stderr, "Caching database %s does not exist" % options.db
-		sys.exit(1)
+	# if options.db == None:
+		# parser.error("Path to caching database")
+	# if not os.path.exists(options.db):
+		# print >>sys.stderr, "Caching database %s does not exist" % options.db
+		# sys.exit(1)
+	try:
+		configfile = open(options.cfg, 'r')
+	except:
+		parser.error("Configuration file not readable")
+	config.readfp(configfile)
+	configfile.close()
 
-	conn = sqlite3.connect(options.db)
-	c = conn.cursor()
+	for section in config.sections():
+		if section == "extractconfig":
+			try:
+				postgresql_user = config.get(section, 'postgresql_user')
+				postgresql_password = config.get(section, 'postgresql_password')
+				postgresql_db = config.get(section, 'postgresql_db')
 
-	c.execute("create table if not exists scores (stringidentifier text, packages int, score real)")
-	c.execute("create index if not exists scoresindex on scores(stringidentifier)")
-	conn.commit()
-	c2 = conn.cursor()
-
-	c.execute("select distinct stringidentifier from stringscache")
-	programstrings = c.fetchmany(10000)
-	while programstrings != []:
-		for p in programstrings:
-			pkgs = {}
-			filenames = {}
-
-			pfs = c2.execute("select package, filename from stringscache where stringidentifier=?", p).fetchall()
-			packages = set(map(lambda x: x[0], pfs))
-
-			if len(packages) == 1:
-				score = float(len(p[0]))
-			else:
-				for pf in pfs:
-					(package, filename) = pf
-					if not filenames.has_key(filename):
-						filenames[filename] = [package]
-					else:   
-						filenames[filename] = list(set(filenames[filename] + [package]))
+				## check to see if a host (IP-address) was supplied either
+				## as host or hostaddr. hostaddr is not supported on older
+				## versions of psycopg2, for example CentOS 6.6, so it is not
+				## used at the moment.
 				try:
-					score = float(len(p[0])) / pow(alpha, (len(filenames) - 1))
+					postgresql_host = config.get(section, 'postgresql_host')
+				except:
+					postgresql_host = None
+				try:
+					postgresql_hostaddr = config.get(section, 'postgresql_hostaddr')
+				except:
+					postgresql_hostaddr = None
+
+				## check to see if a port was specified. If not, default to 'None'
+				try:
+					postgresql_port = config.get(section, 'postgresql_port')
 				except Exception, e:
-					score = len(p[0]) / sys.maxint
-				## cut off for for example postgresql
-				if score < 1e-37:
-					score = 0.0
-			c2.execute("insert into scores(stringidentifier, packages, score) values (?,?,?)", (p[0], len(packages), float(score)))
+					postgresql_port = None
+			except:
+				print >>sys.stderr, "Database connection not defined in configuration file. Exiting..."
+				sys.stderr.flush()
+				sys.exit(1)
+		if section == "supportedlanguages":
+			try:
+				supported_languages = set()
+				if config.get(section, 'C'):
+					supported_languages.add('c',)
+				if config.get(section, 'CSharp'):
+					supported_languages.add('csharp',)
+				if config.get(section, 'Java'):
+					supported_languages.add('java',)
+				if config.get(section, 'JavaScript'):
+					supported_languages.add('javascript',)
+				if config.get(section, 'Php'):
+					supported_languages.add('php',)
+				if config.get(section, 'Python'):
+					supported_languages.add('python',)
+				if config.get(section, 'Ruby'):
+					supported_languages.add('ruby',)
+				if config.get(section, 'ActionScript'):
+					supported_languages.add('actionscript',)
+			except Exception, e:
+				print >>sys.stderr, "Malformed configuration file. Exiting..."
+				print e
+				sys.stderr.flush()
+				sys.exit(1)
+		
+	try:
+		conn = psycopg2.connect(database=postgresql_db, user=postgresql_user, password=postgresql_password, host=postgresql_host, port=postgresql_port)
+
+		c = conn.cursor()
+		c2 = conn.cursor()
+	except:
+		print >>sys.stderr, "Database not running or misconfigured"
+		sys.exit(1)
+		
+
+	for language in supported_languages:
+		query = "create table if not exists scores_%s (stringidentifier text, packages int, score real)" % (language)
+		c.execute(query)
+		query = "create index if not exists scoresindex on scores_%s(stringidentifier)" % (language)
+		c.execute(query)
+		conn.commit()
+
+		query = "select distinct stringidentifier from stringscache_%s" % (language)
+		c.execute(query)
 		programstrings = c.fetchmany(10000)
+		while programstrings != []:
+			for p in programstrings:
+				pkgs = {}
+				filenames = {}
+
+				query = "select distinct package, filename from stringscache_" + language + " where stringidentifier=%s"
+				c2.execute(query,  p)
+				pfs = c2.fetchall()
+				packages = set(map(lambda x: x[0], pfs))
+
+				if len(packages) == 1:
+					score = float(len(p[0]))
+				else:
+					for pf in pfs:
+						(package, filename) = pf
+						if not filenames.has_key(filename):
+							filenames[filename] = [package]
+						else:   
+							filenames[filename] = list(set(filenames[filename] + [package]))
+					try:
+						score = float(len(p[0])) / pow(alpha, (len(filenames) - 1))
+					except Exception, e:
+						score = len(p[0]) / sys.maxint
+					## cut off for for example postgresql
+					if score < 1e-37:
+						score = 0.0
+				query = "insert into scores_" + language + "(stringidentifier, packages, score) values (%s,%s,%s)"
+				c2.execute(query, (p[0], len(packages), float(score)))
+			programstrings = c.fetchmany(10000)
 	conn.commit()
 	c2.close()
-	print "vacuuming"
-	c.execute("vacuum")
+	# print "vacuuming"
+	# c.execute("vacuum")
 	conn.commit()
 	c.close()
 	conn.close()
