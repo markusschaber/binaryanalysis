6ec2d3ec2ddbe156157e08cd6da1a7e007eb2d10
Matthei52 <matthei@hotmail.de>
2017-10-20 12:37:22 +0200

    Added a excecutable file text.py to pretty print reports. This file internal executes bat-scan. TODO: Maybe modify it to be run as postrunscan

---
+++ src/test.py
@@ -0,0 +1,310 @@
+#!/usr/bin/python
+
+import os, json, sys
+import tarfile
+from glob import glob
+from optparse import OptionParser
+from operator import itemgetter
+import ConfigParser
+import subprocess
+import gzip
+
+### unpack the tar.gz archives which are in the outputdir ###
+def unpack_results(rootpath):
+    #tar_files=glob(rootpath+'*.tar.gz')
+    tar_files=[]
+    for root, subFolders, files in os.walk(rootpath):
+        for file in files:
+            tar_files.append(os.path.join(root,file))
+    folders=[]
+    for archive in tar_files:
+        base = os.path.basename(archive)
+        unpackdir = os.path.splitext(os.path.splitext(base)[0])[0]
+        tar = tarfile.open(archive)
+        folder=os.path.join(os.path.dirname(archive), unpackdir)
+        folders.append(folder)
+        tar.extractall(path=folder)
+        tar.close()
+    return folders
+### the rootdir of the scan-output###
+
+def main(argv):
+    config = ConfigParser.ConfigParser()
+    parser = OptionParser()
+    parser.add_option("-b", "--binary", action="store", dest="fw", help="path to binary file", metavar="FILE")
+    parser.add_option("-c", "--config", action="store", dest="cfg", help="path to configuration file", metavar="FILE")
+    parser.add_option("-o", "--outputfile", action="store", dest="outputfile", help="path to output file", metavar="FILE")
+    parser.add_option("-d", "--directory", action="store", dest="fwdir", help="path to directory with files to be scanned", metavar="DIR")
+    parser.add_option("-u", "--outputdir", action="store", dest="outdir", help="path to directory to write results to", metavar="DIR")
+    parser.add_option("-v", "--version", action="store_true", dest="version", help="print version of BAT", metavar="VERSION")
+    parser.add_option("-l", "--blacklist", action="store", dest="blacklist", help="path to blacklist file", metavar="FILE")
+    parser.add_option("-f", "--fileblacklist", action="store", dest="fileblacklist", help="path to file-blacklist file", metavar="FILE")
+    parser.add_option("-w", "--whitelist", action="store", dest="whitelist", help="path to whitelist file", metavar="FILE")
+    parser.add_option("-r", "--result", action="store", dest="respath", help="path to result-file", metavar="DIR")
+    
+    (options, args) = parser.parse_args()
+    
+    ### remove from argv what bat doesn't want to know
+    if '-r' in argv[1:]:
+        del argv[argv.index('-r')+1]
+        del argv[argv.index('-r')]
+        
+    if '-f' in argv[1:]:
+        del argv[argv.index('-f')+1]
+        del argv[argv.index('-f')]
+        
+    if '--fileblacklist' in argv[1:]:
+        del argv[argv.index('--fileblacklist')+1]
+        del argv[argv.index('--fileblacklist')]
+
+    ### indicates if there was any match to a new Package not in whitelist ###
+    bnewPkgs = False
+    ### read the packages from the whitelist.json ###
+    whitelist_file = open(options.whitelist, 'r')
+    whitelist = []
+    whitelist_json = json.load(whitelist_file)["whitelist"]
+    for entry in whitelist_json:
+        whitelist.append(entry['package'])
+    whitelist=set(whitelist)
+    
+    ### file-blacklist ###
+    fblacklist_file = open(options.fileblacklist, 'r')
+    fblacklist = []
+    fblacklist_json = json.load(fblacklist_file)["blacklist"]
+    for entry in fblacklist_json:
+        fblacklist.append(entry['filename'])
+    fblacklist=set(fblacklist)
+    
+    returncode=subprocess.call(['/home/mschnelzer/share/merge/bat-scan']+ argv[1:])
+    
+    rootdir = options.outdir
+    folders = unpack_results(rootdir)
+    scandata = {}
+    report = []
+    
+    ### store here all packages which are not in the whitelist; package: list of checksums ###
+    ### we could need the checksum of the file in which the package was found to make further decisions. This will also be in the output-table ### 
+    packages_niw={}
+    packages_w={}
+    matched_str={}
+    matched_strPerPkg={}
+    matched_pkgs_w = set()
+    matched_pkgs_niw = set()
+    
+    ### read all scandata.json to create a matching-dict from hash-value to filename and path inside the extracted scanresult ####
+    for folder in folders:
+        files=glob(folder+'/*.json')
+        for filepath in files:
+            data_file = open(filepath)
+            data = json.load(data_file)
+            for e in data:
+                if 'checksum' in e:
+                    entry = {'name': e['name'], 'realpath':os.path.join(folder,'data') }
+                    scandata[e['checksum']]=entry
+                
+    ### read all json files containing the file-reports to a list ###
+    for folder in folders:
+        path=os.path.join(folder, "reports")
+        if os.path.isdir(path):
+            files=glob(path+'/*.json.gz')
+            for filepath in files:
+                data_file = gzip.open(filepath, 'rb')
+                #data_file = open(filepath)
+                data = json.load(data_file)
+                entry = {'checksum':os.path.splitext(os.path.splitext(os.path.basename(filepath))[0])[0], 'data':data}
+                report.append(entry)
+    
+    ### prepare data needed for the report### 
+    for result in report:
+        if scandata[result['checksum']]['name'] in fblacklist:
+            continue
+        
+        matched_pkgs=set()
+        
+
+        
+        if 'data' in result and 'ranking' in result['data'] and 'stringresults' in result['data']['ranking'] and 'reports' in result['data']['ranking']['stringresults']:
+            for match in result['data']['ranking']['stringresults']['reports']:
+                matched_pkgs.add(match['packagename'])
+                if match['packagename'] not in matched_strPerPkg:
+                        matched_strPerPkg[match['packagename']]={'unique':set(),'nonunique':set()}
+                ### store the unique matches of the packages ###
+                if 'unique' in match:
+                    str_res = []
+                    for unique in match['unique']:
+                        str_res.append(unique['identifier'])
+                    if result['checksum'] not in matched_str:
+                        matched_str[result['checksum']]={}  
+                    matched_str[result['checksum']][match['packagename']]={}
+                    matched_str[result['checksum']][match['packagename']]['unique'] = str_res
+                    matched_strPerPkg[match['packagename']]['unique'].update(str_res)
+                    
+            ### store the nonunique matches of the packages ###
+            if 'nonUniqueMatches' in result['data']['ranking']['stringresults']:
+                for nonunique in result['data']['ranking']['stringresults']['nonUniqueMatches']:
+                    if result['checksum'] not in matched_str:
+                        matched_str[result['checksum']]={}
+                    if nonunique['packagename'] not in matched_str[result['checksum']]:
+                        matched_str[result['checksum']][nonunique['packagename']]={}
+                    matched_str[result['checksum']][nonunique['packagename']]['nonunique'] = nonunique['nonuniquelines'] 
+                    matched_strPerPkg[nonunique['packagename']]['nonunique'].update(nonunique['nonuniquelines'])
+            
+            matched_pkgs_niw = matched_pkgs - whitelist
+            matched_pkgs_w= matched_pkgs & whitelist
+            
+            ### store for all packages which are not in the whitelist the hash of the file in which the package matched###
+            if len(matched_pkgs_niw)>0:
+                bnewPkgs = True
+                for pkg in matched_pkgs_niw:
+                    if pkg in packages_niw:
+                        packages_niw[pkg].append(result['checksum'])
+                    else:
+                        packages_niw[pkg]=[result['checksum']]
+           
+            ### store for all packages which are in the whitelist the hash of the file in which the package matched###             
+            if len(matched_pkgs_w)>0:
+                for pkg in matched_pkgs_w:
+                    if pkg in packages_w:
+                        packages_w[pkg].append(result['checksum'])
+                    else:
+                        packages_w[pkg]=[result['checksum']]
+    
+    sscanres=''
+    if bnewPkgs:
+        sscanres='Failure'
+    else:
+        sscanres='Success'
+        
+    bottomLimit_pkg_list = []
+    ### sort the packages in descending order of found unique matches, nonunique matches
+    sorted_pkg_list=[]
+    for i in matched_strPerPkg:
+        if i in packages_niw:
+            res = (i,len(matched_strPerPkg[i]['unique']),len(matched_strPerPkg[i]['nonunique']))
+            if (len(matched_strPerPkg[i]['unique'])+len(matched_strPerPkg[i]['nonunique'])) < 10:
+                bottomLimit_pkg_list.append(res)
+            else:
+                sorted_pkg_list.append(res)
+    sorted_pkg_list = sorted(sorted_pkg_list, key=itemgetter(1,2), reverse=True)
+    
+    
+    ### Generate HTML-output ###
+    html = '''<html>
+        <head><style type="text/css">
+            .tg  {border-collapse:collapse;border-spacing:0;}
+            td{font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;}
+            th{font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;}
+        </style></head>
+        <body>
+        Scanresult: %s</br></br>''' %sscanres
+    
+    html+='%s whitelist-packages matched</br>%s packages matched which are not in the whitelist</br></br>' % (len(packages_w), len(sorted_pkg_list))
+    
+    html+='%s packages skipped in the result because of the bottom limit of 10 matches per package</br></br>' % (len(bottomLimit_pkg_list))
+    
+    html+='Following packages matched and have an entry in the whitelist:</br><ul>'
+    for i in packages_w:
+        html+='<li>%s</li>' % i
+    html+='</ul></br>'
+    
+    html+='Following packages matched but have no entry in the whitelist:</br>(For more informations see the details)<ul>'
+    if len(sorted_pkg_list)>0:
+        html += '<table><tr><th>Package</th><th>Unique</th><th>Nonunique</th></tr>'
+    for i in sorted_pkg_list:
+        link='<a href="#%s">%s</a>' %(i[0], i[0])
+        html+='<tr><td><li>%s</li></td><td>%s</td><td>%s</td></tr>' % (link,i[1],i[2])
+    if len(sorted_pkg_list)>0:
+        html+='</table></br>Detailed scanresults:</br>'
+    
+    for i in sorted_pkg_list:
+        html+='''</br>
+                <table id="%s" style="border-style:solid">
+                    <tr>
+                        <td>Package</td>
+                        <td>%s</td>
+                    </tr>
+                    <tr>
+                        <td style="background-color:#c0c0c0; padding:2px;" colspan="2"></td>
+                    </tr>
+                    <tr>
+                        <th colspan="2">Found in following files</th>
+                    </tr>'''% (i[0], i[0])
+        for j in packages_niw[i[0]]:
+            html+=''' <tr>
+                        <td>Filename</td>
+                        <td>%s</td>
+                      </tr>
+                      <tr>
+                        <td>Path:</td>
+                        <td>%s</td>
+                      </tr>
+                      <tr>
+                        <td>More informations</td>
+                        <td><a href="%s">%s</a></td>
+                      <tr>
+                        <td style="border:none; padding:0px;" colspan="2"><hr></td>
+                      </tr>
+                      ''' % (scandata[j]['name'], os.path.relpath(scandata[j]['realpath'],options.respath), os.path.relpath(os.path.join(os.path.split(scandata[j]['realpath'])[0],'reports',j+'.json'),options.respath), os.path.relpath(os.path.join(os.path.split(scandata[j]['realpath'])[0],'reports',j+'.json'),options.respath))
+        html+='''<tr>
+                     <td style="background-color:#c0c0c0; padding:2px;" colspan="2"></td>
+                 </tr>
+                 <tr>
+                     <th colspan="2">Matches</th>
+                 </tr>'''
+        
+        html+='''<tr>
+        <td colspan="2">Unique:(%s)</br><ul>''' % len(matched_strPerPkg[i[0]]['unique'])
+        
+        for j in list(matched_strPerPkg[i[0]]['unique'])[:10]:
+            html += '<li>%s</li>' % j
+        if len(matched_strPerPkg[i[0]]['unique'])>10:
+            html += '<li>...</li>'
+        html+='</ul></br>Nonunique: (%s)</br><ul>' % len(matched_strPerPkg[i[0]]['nonunique'])   
+        for j in list(matched_strPerPkg[i[0]]['nonunique'])[:10]:
+            html += '<li>%s</li>' % j
+        if len(matched_strPerPkg[i[0]]['nonunique'])>10:
+            html += '<li>...</li>'
+        html+='</ul></table></br></br>'
+    html+='</body></html>'
+    
+    ### end generate html output ### 
+    
+    ### write the html-file to the desired output dir ###
+    outputdata=open(os.path.join((str)(options.respath),'output.html'), 'w')
+    outputdata.write(html)
+    outputdata.flush()
+    outputdata.close()
+    
+    ### generate json output ###
+    json_out = {}
+
+    for pkg in matched_strPerPkg:
+        json_out[pkg]={}
+        json_out[pkg]['unique']=[]
+        json_out[pkg]['nonunique']=[]
+        for identifier in matched_strPerPkg[pkg]['unique']:
+            entry={}
+            entry['identifier']=identifier
+            entry['comment']=""
+            json_out[pkg]['unique'].append(entry)
+        for identifier in matched_strPerPkg[pkg]['nonunique']:
+            entry={}
+            entry['identifier']=identifier
+            entry['comment']=""
+            json_out[pkg]['nonunique'].append(entry)    
+    json_out['skipped']= [pkg for pkg in bottomLimit_pkg_list]
+    
+    ### write the json-file to the desired output dir ###
+    outputdata=open(os.path.join((str)(options.respath),'output.json'), 'w')
+    outputdata.write(json.dumps(json_out,sort_keys=True,indent=4,separators=(',',':')))
+    outputdata.flush()
+    outputdata.close()
+    
+    if bnewPkgs:
+        sys.exit(1)
+    else:
+        sys.exit(0)
+        
+if __name__ == "__main__":
+        main(sys.argv)
+    
